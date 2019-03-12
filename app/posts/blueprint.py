5ac61daf80b46b63efe0d759f3596c197da40484
Kiryakov Denis <KiryakovDenis@gmail.com>
2019-03-12 16:59:08 +0500

    added create form for posts

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -1,15 +1,53 @@
 from flask import Blueprint
 from flask import render_template
-from models import Post
+from flask import request
+from flask import redirect
+from flask import url_for
+
+from app import db
+from models import Post, Tag
+from .forms import PostForm
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
+@posts.route('/create', methods=['POST','GET'])
+def create_post():
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+
+        try:
+            post = Post(title=title, body=body)
+            db.session.add(post)
+            db.session.commit()
+        except:
+            print('Ошибка создания объекта post')
+        
+        return redirect(url_for('posts.index'))
+    
+    form = PostForm()
+    return render_template('posts/create_post.html', form=form)
+
 @posts.route('/')
 def index():
-    posts = Post.query.all()
+    search = request.args.get('search')
+
+    if search:
+        posts = Post.query.filter(Post.title.contains(search) | Post.body.contains(search)).all()
+    else:
+        posts = Post.query.order_by(Post.created.desc())
+
     return render_template('posts/index.html', posts=posts)
 
 @posts.route('/<slug>')
 def post_detail(slug):
     post = Post.query.filter(Post.slug==slug).first()
-    return render_template('posts/post_detail.html', post=post)
+    tags = post.tags
+    return render_template('posts/post_detail.html', post=post, tags=tags)
+
+@posts.route('/tag/<slug>')
+def tag_detail(slug):
+    tag = Tag.query.filter(Tag.slug==slug).first()
+    posts = tag.posts.all()
+    return render_template('posts/tag_detail.html',tag=tag, posts=posts)
