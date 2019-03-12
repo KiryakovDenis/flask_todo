86917e3ddba090fade8af97d09a269de594f6ef7
Denis Kiryakov <kiryakovDenis@gmail.com>
2019-03-12 23:08:10 +0500

    adding paginations

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -27,18 +27,41 @@ def create_post():
         return redirect(url_for('posts.index'))
     
     form = PostForm()
-    return render_template('posts/create_post.html', form=form)
+    return render_template('posts/edit_post.html', form=form)
+
+@posts.route('/<slug>/edit', methods=['POST','GET'])
+def edit_post(slug):
+    post = Post.query.filter(Post.slug == slug).first()
+
+    if request.method == 'POST':
+        form = PostForm(fomrdata=request.form, obj=post)
+        form.populate_obj(post)
+        db.session.add(post)
+        db.session.commit()
+
+        return redirect(url_for('posts.post_detail', slug=slug))
+    
+    form = PostForm(obj=post)
+    return render_template('posts/edit.html', post=post, form=form)
 
 @posts.route('/')
 def index():
     search = request.args.get('search')
 
+    page = request.args.get('page')
+    if page and page.isdigit():
+        page = int(page)
+    else:
+        page = 1
+
     if search:
-        posts = Post.query.filter(Post.title.contains(search) | Post.body.contains(search)).all()
+        posts = Post.query.filter(Post.title.contains(search) | Post.body.contains(search))
     else:
         posts = Post.query.order_by(Post.created.desc())
 
-    return render_template('posts/index.html', posts=posts)
+    pages = posts.paginate(page=page, per_page=5)
+
+    return render_template('posts/index.html', posts=posts, pages=pages)
 
 @posts.route('/<slug>')
 def post_detail(slug):
